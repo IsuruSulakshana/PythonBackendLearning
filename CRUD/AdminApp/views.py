6eaacafaabd3c253ad699896ad8d0a45405e96c6
Isuru Sulakshana <pererawhis.20@uom.lk>
2023-12-16 23:28:05 +0530

    updated

--- CRUD/AdminApp/views.py
+++ CRUD/AdminApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def posts(request):
+    return render(request,'posts.html')
+
+def post(request,pid):
+    return render(request,'post.html')
+
